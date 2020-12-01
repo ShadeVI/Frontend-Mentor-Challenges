e623f5b2db3462305ae7f1d151255329890c2697
ShadeVI <manuel.t.shade@gmail.com>
2020-12-01 13:03:41 +0000

    Restyle index.html

--- script.js
+++ script.js
@@ -33,20 +33,30 @@ const db = [
     },
 ];
 
-const newbieList = document.querySelector(".newbieList");
-const juniorList = document.querySelector(".juniorList");
+const newbieSection = document.querySelector("#newbie .box-container");
+const juniorSection = document.querySelector("#junior .box-container");
 
 db.forEach(({ name, level }) => {
     const nameFormatted = formatName(name);
-    const listItem = document.createElement("li");
+    const listItem = document.createElement("div");
+    listItem.classList.add("box");
     listItem.innerHTML = `
-        <a href="/${name}/index.html">${nameFormatted}</a> - 
-        <a href="https://github.com/ShadeVI/Frontend-Mentor-Challenges/tree/main/${name}">View Code</a>
+        <div class="img-wrapper">
+            <img
+                src="./${name}/design/desktop-design.jpg"
+                alt=""
+            />
+        </div>
+        <div class="info">
+            <h3 class="title"><a href="/${name}/index.html">${nameFormatted}</a></h3>
+            <a class="code" href="https://github.com/ShadeVI/Frontend-Mentor-Challenges/tree/main/${name}"><i class="fab fa-github"></i></a>
+            <p class="level">${level}</p>
+        </div>
     `;
     if (level === "Newbie") {
-        newbieList.appendChild(listItem);
+        newbieSection.appendChild(listItem);
     } else if (level === "Junior") {
-        juniorList.appendChild(listItem);
+        juniorSection.appendChild(listItem);
     }
 });
 
