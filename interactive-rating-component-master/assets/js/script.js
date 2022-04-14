2df8a215416a0e375305ab05c0f263e858f4ea4e
ShadeVI <manuel.t.shade@gmail.com>
2022-04-14 14:31:56 +0100

    added modal

--- interactive-rating-component-master/assets/js/script.js
+++ interactive-rating-component-master/assets/js/script.js
@@ -2,7 +2,18 @@ const formEl = document.querySelector(".form");
 
 formEl.addEventListener("submit", (e) => {
   const formData = new FormData(formEl);
-  if (!formData.has("rate")) return e.preventDefault();
+  if (!formData.has("rate")) {
+    showModal();
+    return e.preventDefault();
+  }
 
   sessionStorage.setItem("rate", formData.get("rate"));
 });
+
+function showModal() {
+  const modalEl = document.querySelector(".modal");
+  modalEl.classList.toggle("showModal");
+  setTimeout(() => {
+    modalEl.classList.toggle("showModal");
+  }, 2500);
+}
